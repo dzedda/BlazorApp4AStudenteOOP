c4d7a861664e25ee12818a303b075cac7133906f
davide <davide@WORKSTATION10>
2023-10-25 10:58:27 +0200

    Aggiungere i file di progetto.

---
+++ BlazorApp4A/Models/Borsista.cs
@@ -0,0 +1,25 @@
+﻿namespace BlazorApp4A.Models
+{
+    public class Borsista : Studente
+    {
+        private decimal borsa;        
+
+        public Borsista(string Nome, string Sezione,decimal Borsa) : base(Nome, Sezione)
+        {
+            borsa = Borsa;
+        }
+
+        public override void Diplomati()
+        {
+            base.Diplomati();
+            borsa = 0;
+        }
+
+
+        public decimal Borsa
+        {
+            get { return borsa; }
+            set { borsa = value; }
+        }
+    }
+}
---
+++ BlazorApp4A/Models/Studente.cs
@@ -0,0 +1,55 @@
+﻿namespace BlazorApp4A.Models
+{
+    public class Studente
+    {
+		private string nome;
+		private byte anno;
+		private string sezione;
+
+		private bool diplomato;
+
+        public Studente(string Nome, string Sezione)
+        {
+			nome = Nome;
+			sezione=Sezione;
+			anno = 1;
+			diplomato = false;
+        }
+
+        public void Promuovi()
+		{
+			anno++;
+		}
+
+		public virtual void Diplomati()
+		{
+			diplomato= true;
+		}
+
+
+		public bool Diplomato
+		{
+			get { return diplomato; }
+			
+		}
+
+		public string Sezione
+		{
+			get { return sezione; }
+			
+		}
+
+
+		public byte Anno
+		{
+			get { return anno; }
+		}
+
+
+		public string Nome
+		{
+			get { return nome; }			
+		}
+
+	}
+}
