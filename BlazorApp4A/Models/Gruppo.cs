9f16b31b21645971f65a2e6896c3fd1f9b11a8ca
davide <davide@WORKSTATION10>
2023-11-15 18:52:41 +0100

    Aggiunta classe gruppo con lista studenti e borsisti

---
+++ BlazorApp4A/Models/Gruppo.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace BlazorApp4A.Models
+{
+    public class Gruppo
+    {
+
+        private static string dataSourceString = @"Data/Source/gruppo.json";
+        public List<Studente> StudenteList { get; set; } = new List<Studente>();
+        public List<Borsista> BorsistaList { get; set; } = new List<Borsista>();
+
+        public virtual void Serializza()
+        {
+            File.WriteAllText(dataSourceString, JsonConvert.SerializeObject(this));
+        }
+
+        public static Gruppo Deserializza()
+        {
+            Gruppo? x;
+            x = JsonConvert.DeserializeObject<Gruppo>(File.ReadAllText(dataSourceString));
+            return x;
+
+        }
+    }
+}
