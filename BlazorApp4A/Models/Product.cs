6ce59a22ef3bcf71771f4537a145b4fae43b25d5
Davide Zedda <davide.zedda@giua.edu.it>
2023-11-25 16:44:49 +0100

    aggiunto product list

---
+++ BlazorApp4A/Models/Product.cs
@@ -0,0 +1,36 @@
+﻿using BlazorApp4A.Pages;
+using Newtonsoft.Json;
+
+namespace BlazorApp4A.Models
+{
+    
+
+    public class ProductList
+    {
+        private static string dataSourceString = @"Data/Source/products.json";
+
+        public List<Product>  Products { get; set; }
+        public virtual void Serializza()
+        {
+            File.WriteAllText(dataSourceString, JsonConvert.SerializeObject(this));
+        }
+        public static ProductList Deserializza()
+        {
+            ProductList? x;
+            x = JsonConvert.DeserializeObject<ProductList>(File.ReadAllText(dataSourceString));
+            return x;
+        }
+    }
+
+    public class Product
+    {
+        public string id { get; set; }
+        public string nome { get; set; }
+        public string supplierId { get; set; }
+        public string CategoryId { get; set; }
+        public string Unit { get; set; }
+        public string Price { get; set; }
+    }
+
+
+}
