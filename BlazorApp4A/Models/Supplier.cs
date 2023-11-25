8e80aaeef810ff4c999773e4b7b8bee8044472c5
davide <davide@WORKSTATION10>
2023-11-25 08:02:16 +0100

    Aggiunto json suppliers e products e classe e pagina listsuppliers

---
+++ BlazorApp4A/Models/Supplier.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+namespace BlazorApp4A.Models
+
+{
+    public class SupplierList
+    {
+        private static string dataSourceString = @"Data/Source/suppliers.json";
+        public List<Supplier> Suppliers { get; set; }= new List<Supplier>();
+        public virtual void Serializza()
+        {
+            File.WriteAllText(dataSourceString, JsonConvert.SerializeObject(this));
+        }
+        public static SupplierList Deserializza()
+        {
+            SupplierList? x;            
+            x = JsonConvert.DeserializeObject<SupplierList>(File.ReadAllText(dataSourceString));            
+            return x;
+        }
+    }
+    public class Supplier
+    {
+        public string IdSupplier { get; set; }
+        public string Name { get; set; }
+        public string Contact { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string PostaCode { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+    }
+
+
+   
+
+
+}
